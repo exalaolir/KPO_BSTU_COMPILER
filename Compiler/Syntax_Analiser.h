315348cd5712c7fc807ebcf4b2336f44902b0b07
exalaolir <alexbarilo14@gmail.com>
2024-12-01 23:29:12 +0300

    add

--- Compiler/Syntax_Analiser.h
+++ Compiler/Syntax_Analiser.h
@@ -17,7 +17,7 @@ namespace ANALISER
 		void checkFun(std::vector<Lexem>& lexTable, IdTable& idTable, size_t& index);
 		void checkExp(std::vector<Lexem>& lexTable, IdTable& idTable, size_t& index, Entry& currentType, char endSymbol = ';');
 		void checkReturnType(std::vector<Lexem>& lexTable, IdTable& idTable, size_t& index);
-		void checkBlockRange(std::vector<Lexem>& lexTable, IdTable& idTable, size_t index, size_t& leftEnd, size_t& rightStart, size_t& rightEnd);
+		void checkBlockRange(std::vector<Lexem>& lexTable, IdTable& idTable, size_t index, size_t& leftStart, size_t& leftEnd, size_t& rightStart, size_t& rightEnd);
 
 		static inline std::unordered_map<Keywords, string> types 
 		{
